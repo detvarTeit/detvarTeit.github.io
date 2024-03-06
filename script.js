c1f4abdfdc1d1e074d207c3b77dfb72843a321e9
Lyx <130296296+Lyxminxx@users.noreply.github.com>
2024-03-06 10:44:17 +0100

    sizing problems fixed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 GHBVideo = document.getElementById("GHBVid");
-document.getElementById("test").onclick = function(){
-    document.getElementById("playArea").style.width = "0%" 
-    document.getElementById("GHBVid").style.width = "30%" 
+document.getElementById("playArea").onclick = function(){
+    document.getElementById("playArea").style.display = "none"
+    document.getElementById("GHBVid").style.display = "flex"
+
     GHBVideo.play()
+    GHBVideo.onended = function(){
+        document.getElementById("playArea").style.display = "flex"
+        document.getElementById("GHBVid").style.display = "none"
+    }
 }
